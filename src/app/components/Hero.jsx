b5b0b671f480f84cea28d37c4050ad4ff153ac5b
Gregory Robertson <147750592+GregoryRobetertson@users.noreply.github.com>
2024-05-14 03:49:29 +0000

    created hero

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className=' mt-5 text-white'>
+<div className='col-lg-12'>
+    <h1 className='text-4xl'>Hello, my name is Gregory Robertson.</h1>
+    <p>I am a software developer.</p>
+    <p>As a software developer, I design, code, test, and maintain software applications or systems to meet specific requirements and improve functionality.</p>
+    <div>
+        <a className='btn btn-primary me-2' href="">Resume</a>
+        <a  className='btn btn-secondary ' href="">Contact Me</a>
+    </div>
+</div>
+    </div>
+  )
+}
