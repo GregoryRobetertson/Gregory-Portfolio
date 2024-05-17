9c80e3e3755e862bc7bcd3a683181844ff304c18
Gregory Robertson <147750592+GregoryRobetertson@users.noreply.github.com>
2024-05-17 22:28:31 +0000

    updated the styles

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,33 +1,26 @@
 'use client';
-import React from 'react'
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+
 export default function Header() {
   return (
     <div className="mt-0 p-2 bg-violet-900">
-    <nav className="container mx-auto flex justify-end">
-      <Nav activeKey="/home">
-        <Nav.Item>
-          <Nav.Link href="#about" className="text-white px-4 py-2 hover:bg-violet-800">About Me</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-1" href="#skills" className="text-white px-4 py-2 hover:bg-violet-800">Skills</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2" href="#projects" className="text-white px-4 py-2 hover:bg-violet-800">Projects</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-3" href="#contact" className="text-white px-4 py-2 hover:bg-violet-800">Contact</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-4" href="" className="text-white px-4 py-2 hover:bg-violet-800">Resume</Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </nav>
-  </div>
-  
-    
-
-
-   
-  )
+      <Navbar expand="lg" className="container mx-auto">
+        <Navbar.Brand className="text-white text-2xl font-bold">
+          Gregory Robertson
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#about" className="text-white px-4 py-2 hover:bg-violet-800">About Me</Nav.Link>
+            <Nav.Link href="#skills" className="text-white px-4 py-2 hover:bg-violet-800">Skills</Nav.Link>
+            <Nav.Link href="#projects" className="text-white px-4 py-2 hover:bg-violet-800">Projects</Nav.Link>
+            <Nav.Link href="#contact" className="text-white px-4 py-2 hover:bg-violet-800">Contact</Nav.Link>
+            <Nav.Link href="" className="text-white px-4 py-2 hover:bg-violet-800">Resume</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
 }
